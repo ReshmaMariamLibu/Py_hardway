e15c69d01c6a087bd74643e6e6e02de273fdd1f4
Reshma Mariam Libu <reshmamariamlibu@gmail.com>
2024-05-22 14:37:43 +0530

    Created program to print numbers and math

---
+++ ex3.py
@@ -0,0 +1,31 @@
+print("I will now count my chickens:") 
+
+
+#  prints Hens along with this adding 25 and 30 and then dividing the sum by 6
+print("Hens", 25 + 30 / 6) 
+
+#prints Roosters and take the difference of 100 and 25 then multiplying the diff with 3 and taking modulus by 4
+print("Roosters", 100 - 25 * 3 % 4) 
+
+print("Now I will count the eggs:")
+
+print("3 + 2 + 1 -5 + 4 % 2-1 / 4 + 6")
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2) 
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+#prints true if 5 is greater than -2 else it prints false
+print("Is it greater?", 5 > -2)
+
+#prints true if 5 is greater than or equal to -2 else it prints false
+print("Is it greater or equal?", 5 >= -2)
+
+#prints true if 5 is less than or equal to -2 else it prints false
+print("Is it less or equal?", 5 <= -2)
