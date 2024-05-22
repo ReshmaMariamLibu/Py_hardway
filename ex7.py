2511455cdaa6b6053303831f6f8758aec0c68d7f
Reshma Mariam Libu <reshmamariamlibu@gmail.com>
2024-05-22 22:27:15 +0530

    More printing Created a program to prints out a rhyme and then concatnates out a word to obtain the word Cheese burger

---
+++ ex7.py
@@ -0,0 +1,22 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow')) # prints the second line of the rhyme, with a placeholder {} that is substituted with the word 'snow' using the format() method.
+print("And everywhere that Mary went.")
+print("." * 10) #prints a period "(.)" ten times, creating a line of dots.
+
+# end1 to end12 these variables store individual letters to spell out the word "Cheeseburger".
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11= "e"
+end12 = "r"
+
+#concatenates and prints the variables to get cheese burger and returns the result
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ') 
+print(end7 + end8 + end9 + end10 + end11 + end12) 
